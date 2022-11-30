44e9da92a387e36c3fc39358da7d798fab0100fd
HKS <78211002+Harsh-1105@users.noreply.github.com>
2022-11-30 17:40:42 +0530

    Add files via upload

---
+++ Blog.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const Blog = () => {
+  return (
+    <><div class="header">
+    <h2>CricNow</h2>
+  </div>
+  
+  <div class="row">
+    <div class="leftcolumn">
+      <div class="card">
+        <h2>Ex Pak Captain Reacts To Wasim Akram's "Treated Me Like A Servant" Claim</h2>
+        <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>India vs New Zealand, 3rd ODI tip-off XI: Sanju Samson unlikely to play, Kuldeep for Yuzvendra Chahal</h2>
+        <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>Dubai Capitals and Abu Dhabi Knight Riders to kickstart inaugural ILT20</h2>
+          <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>India vs New Zealand Highlights: 3rd ODI Called Off Due to Persistent Rains</h2>
+          <h5>30th Nov 2022</h5>
+      </div>
+    </div>
+
+
+    <div class="rightcolumn">
+      <div class="card">
+        <h2>About Me</h2>
+<p>
+  HKS 2010991879
+</p>
+      </div>
+      <div class="card">
+        <h3>Popular Post</h3>
+        <div class="fakeimg">Image</div>
+        <div class="fakeimg">Image</div>
+        <div class="fakeimg">Image</div>
+      </div>
+      <div class="card">
+        <h3>Follow Me</h3>
+        <p>Twitter Facebook Instagram Quora</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <h2>Copyright 2022 HKS</h2>
+  </div>
+    </>
+    
+  );
+};
---
+++ Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      
+<p>
+      <label>Title  : <input type="text" /></label>
+    </p>
+    <p>
+      <label>Author : <input type="text" /></label>
+    </p>
+    <p>
+      <label>Content : <input type="text" /></label>
+    </p>
+    
+
+<p>
+       <button type="submit">Submit</button>
+    </p>
+    </div>
+  );
+};
